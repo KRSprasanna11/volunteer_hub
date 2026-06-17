f7b68531114cdbfe38e6f926af2ab43a88ada1af
K R S Prasanna <prasanna2006krs@gmail.com>
2026-06-17 12:16:13 +0530

    Fix API URL

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://https://volunteer-hub-jp64.onrender.com/api/auth";
+const API_URL = "https://volunteer-hub-jp64.onrender.com/api/auth";
 
 const registerUser = (user) => {
   return axios.post(`${API_URL}/register`, user);
